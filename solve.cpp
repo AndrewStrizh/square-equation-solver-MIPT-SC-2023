f9cafaf4affd349f475e83e6b1c1c50cf052fe78
Андрей Стрижевский <95929618+AndrewStrizh@users.noreply.github.com>
2023-08-28 23:27:35 +0300

    Add files via upload

---
+++ solve.cpp
@@ -0,0 +1,108 @@
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include "solve.h"
+
+
+int equate_zero(const double x)
+{
+    assert(isfinite(x));
+
+    return fabs(x - 0) < EPS_ALL;
+}
+
+
+ROOT_KEYS solve_square(const struct Params p, struct Roots* r)
+{
+    assert(isfinite(p.a));
+    assert(isfinite(p.b));
+    assert(isfinite(p.c));
+    assert(r);
+    assert(isfinite(r->x1));
+    assert(isfinite(r->x2));
+
+    if (equate_zero(p.a))
+        return solve_linear(p, r);
+    else
+    {
+        double aa = p.a * 2;
+        const double discr = p.b * p.b - 4 * p.a * p.c;
+        if (equate_zero(discr))
+        {
+            if (equate_zero(p.b))
+                r->x1 = p.b;
+            else
+                r->x1 = -p.b / aa;
+            return ONE_ROOT;
+        }
+        else if (discr > EPS_ALL)
+        {
+            const double sq_discr = sqrt(discr);
+            r->x1 = (-p.b - sq_discr) / aa;
+            r->x2 = (-p.b + sq_discr) / aa;
+            return TWO_ROOTS;
+        }
+        else
+            return ZERO_ROOTS;
+    }
+}
+
+
+ROOT_KEYS solve_linear(const struct Params p, struct Roots* r)
+{
+    assert(r);
+    assert(isfinite(r->x1));
+    assert(isfinite(p.b));
+    assert(isfinite(p.c));
+
+    if (equate_zero(p.b))
+    {
+        if (equate_zero(p.c))
+            return INF_ROOTS;
+        else
+            return ZERO_ROOTS;
+    }
+    else
+    {
+        if (equate_zero(p.c))
+            r->x1 = p.c;
+        else
+            r->x1 = -p.c / p.b;
+        return ONE_ROOT;
+    }
+}
+
+int print_solution(const struct Params params, struct Roots* roots)
+{
+    assert(roots);
+    assert(isfinite(params.a));
+    assert(isfinite(params.b));
+    assert(isfinite(params.c));
+    assert(isfinite(roots->x1));
+    assert(isfinite(roots->x2));
+
+    int nRoots = solve_square(params, roots);
+    switch (nRoots)
+    {
+        case ZERO_ROOTS:
+            printf(SMALL_ERROR_COLOR("There are no roots\n"));
+            break;
+
+        case ONE_ROOT:
+            printf(CORRECT_COLOR("x = %lf\n"), roots->x1);
+            break;
+
+        case TWO_ROOTS:
+            printf(CORRECT_COLOR("x1 = %lf\tx2 = %lf\n"), roots->x1, roots->x2);
+            break;
+
+        case INF_ROOTS:
+            printf(SMALL_ERROR_COLOR("Number of roots: infinity\n"));
+            break;
+
+        default:
+            break;
+    }
+
+    return 0;
+}
---
+++ solve.h
@@ -0,0 +1,88 @@
+/**
+ *\file
+ *File solve.cpp containing functions for solving linear and quadratic equations
+ */
+
+
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+
+
+#define ERROR_COLOR(str) RED_TEXT str RESET_COLOR
+
+#define CORRECT_COLOR(str) GREEN_TEXT str RESET_COLOR
+
+#define SMALL_ERROR_COLOR(str) YELLOW_TEXT str RESET_COLOR
+
+
+#define RED_TEXT "\x1b[31m"
+#define GREEN_TEXT "\x1b[32m"
+#define YELLOW_TEXT "\x1b[33m"
+#define RESET_COLOR "\x1b[0m"
+
+const double EPS_ALL = 1e-6; /**< Accuracy of the numerical data type */
+
+
+/**
+ *\enum
+ * Possible number of roots
+ */
+enum ROOT_KEYS
+{
+    ZERO_ROOTS,
+    ONE_ROOT,
+    TWO_ROOTS,
+    INF_ROOTS
+};
+
+
+/**
+ *Function for solving a quadratic equation
+ *\param p - structure stores the parameters of the quadratic equation
+ *\param *r - address of the structure storing the values of the roots of a quadratic equation
+ */
+ROOT_KEYS solve_square(const struct Params p, struct st_roots* r);
+
+
+/**
+ *Function for solving a quadratic equation
+ *\param p - structure stores the parameters of the quadratic equation
+ *\param *r - address of the structure storing the values of the roots of a quadratic equation
+ */
+ROOT_KEYS solve_linear(const struct Params p, struct Roots* r);
+
+
+/**
+ *Function for comparing double number and zero
+ *\param x - compared numbers
+ */
+int equate_zero(const double x);
+
+
+/**
+ *The main function solving the quadratic equation
+ *\param params - structure storing the parameters of a quadratic equation
+ *\param *roots - address of the structure storing the values of the roots of a quadratic equation
+ */
+int print_solution(const struct Params params, struct Roots* roots);
+
+
+/**
+ *\brief This structure stores the parameters of the quadratic equation
+ */
+struct Params
+{
+    double a, b, c;
+};
+
+
+/**
+ *\brief This structure storing the values of the roots of a quadratic equation
+ */
+struct Roots
+{
+    double x1, x2;
+};
+
+
+#endif // HEADER_H_INCLUDED
