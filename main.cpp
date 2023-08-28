448592a4aaf5c42aa57fc2e392278d867c96c6dd
Андрей Стрижевский <95929618+AndrewStrizh@users.noreply.github.com>
2023-08-28 23:16:32 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "ses.h"
+#include <errno.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+    if (argc > 2 && !strcmp(argv[1], "tests") && (access(argv[2], F_OK) == 0))
+        all_tests(argv[2]);
+    else if (argc > 2)
+        printf("No such file in directory\n");
+    else
+    {
+        printf("Square equation solver\n"
+               "# (c) STA, 2023\n\n");
+
+        struct Params params = { 0, 0, 0 };
+        struct Roots roots = { 0, 0 };
+
+        init_params(&params);
+        print_solution(params, &roots);
+    };
+}
---
+++ ses.h
@@ -0,0 +1,76 @@
+/**
+ *\file
+ * File main.cpp - file with the main function
+ */
+
+
+#ifndef HEADER_H_INCLUDED
+#define HEADER_H_INCLUDED
+
+
+/**
+ *\enum
+ * Possible number of roots
+ */
+enum ROOT_KEYS
+{
+    ZERO_ROOTS,
+    ONE_ROOT,
+    TWO_ROOTS,
+    INF_ROOTS
+};
+
+
+/**
+ *Function initializing the parameters of a quadratic equation
+ *\param *params - structure storing the parameters of a quadratic equation
+ */
+int init_params(struct Params* params);
+
+
+/**
+ *\brief The main function solving the quadratic equation
+ *\param params - structure storing the parameters of a quadratic equation
+ *\param *roots - structure storing the values of the roots of a quadratic equation
+ */
+int print_solution(const struct Params params, struct Roots* roots);
+
+
+/**
+ *Function that starts the execution of tests
+ *\param *filename - the address of the file, name entered by the user
+ */
+int all_tests(const char* filename);
+
+
+/**
+ *\brief This structure stores the parameters of the quadratic equation
+ */
+struct Params
+{
+    double a, b, c;
+};
+
+
+/**
+ *\brief This structure storing the values of the roots of a quadratic equation
+ */
+struct Roots
+{
+    double x1, x2;
+};
+
+
+/**
+ *\brief Structure storing the test parameters of the quadratic equation, the correct roots and
+ *their number
+ */
+struct test_solve_square
+{
+    struct Params p;
+    struct Roots r;
+    int nRootsRef;
+};
+
+
+#endif // HEADER_H_INCLUDED
