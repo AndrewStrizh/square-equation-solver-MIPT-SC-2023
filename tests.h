bf6d6c83912e1f759eb26488fad4230138ef9f2d
Андрей Стрижевский <95929618+AndrewStrizh@users.noreply.github.com>
2023-08-29 23:30:55 +0300

    Update tests.h

--- tests.h
+++ tests.h
@@ -15,22 +15,27 @@
 #define SMALL_ERROR_COLOR(str) YELLOW_TEXT str RESET_COLOR
 
 
-/// Macro that creates the test_solve_square structure and starts testing
-#define TEST_SOLVE_SQUARE(a, b, c, x1, x2, nRoots)                                                 \
-    (                                                                                              \
-        {                                                                                          \
-            struct test_solve_square t = { { a, b, c }, { x1, x2 }, nRoots };                      \
-            run_test_solve_square(t, &correct_answers, &all_answers, tmp);                         \
-        })
+/**
+ *\brief Macro that creates the test_solve_square structure and starts testing
+ */
+#define TEST_SOLVE_SQUARE(a, b, c, x1, x2, nRoots)                                             \
+    ({                                                                                         \
+        struct test_solve_square t = { { a, b, c }, { x1, x2 }, nRoots };                      \
+        run_test_solve_square(t, tmp, &correct_answers, &all_answers);                         \
+    })
 
 
-/// Macro that creates variables for the number of all tests and valid tests
+/**
+ *\brief Macro that creates variables for the number of all tests and valid tests
+ */
 #define BEGIN_TEST                                                                                 \
     int correct_answers = 0;                                                                       \
     int all_answers = 0;
 
 
-/// Macro that outputs variable numbers of all tests and valid tests to the console
+/**
+ *\brief Macro that outputs variable numbers of all tests and valid tests to the console
+ */
 #define END_TEST                                                                                   \
     printf(GREEN_TEXT "passed %d tests out of %d" RESET_COLOR, correct_answers, all_answers);
 
@@ -70,7 +75,7 @@ ROOT_KEYS solve_square(const struct Params p, struct Roots* r);
  *\param p - structure stores the parameters of the quadratic equation
  *\param *r - structure storing the values of the roots of a quadratic equation
  */
-ROOT_KEYS solve_linear(const double b, const double c, double* x1);
+ROOT_KEYS solve_linear(const struct Params p, struct Roots* r);
 
 
 /**
@@ -85,7 +90,7 @@ int equate_double_test(const double x, const double y);
  *Function that checks whether the test results match the results of the solve_square function
  *\param t - structure storing the test parameters of the quadratic equation, the correct roots and
  *their number
- *\param *filename - address of the file, name entered by the user
+ *\param *tmp - address of the file
  *\param *all_answers - address of the number of all tests
  */
 int test_solve(const struct test_solve_square t, FILE* tmp, int* all_answers);
@@ -97,9 +102,10 @@ int test_solve(const struct test_solve_square t, FILE* tmp, int* all_answers);
  *their number
  *\param *correct_answers - address of the number of valid tests
  *\param *all_answers - address of the number of all tests
- *\param *filename - address of the file, name entered by the use
+ *\param *tmp - address of the file
  */
-int run_test_solve_square(const struct test_solve_square t, int* correct_answers, int* all_answers, FILE* tmp);
+int run_test_solve_square(
+    const struct test_solve_square t, FILE* tmp, int* correct_answers, int* all_answers);
 
 
 /**
