f994484d1447249bac02d902199266eeac831d47
Андрей Стрижевский <95929618+AndrewStrizh@users.noreply.github.com>
2023-08-29 23:29:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,11 @@
 #include <unistd.h>
 #include "ses.h"
 
+
 int main(int argc, char* argv[])
 {
+    assert(argv);
+
     if (argc > 2 && !strcmp(argv[1], "tests") && (access(argv[2], F_OK) == 0))
         all_tests(argv[2]);
     else if (argc > 2)
@@ -18,10 +21,30 @@ int main(int argc, char* argv[])
         printf("Square equation solver\n"
                "# (c) STA, 2023\n\n");
 
+
         struct Params params = { 0, 0, 0 };
         struct Roots roots = { 0, 0 };
 
+        char p = 'N';
+
+        printf("Do you want to use uplot?[Y/N] ");
+        scanf("%c", &p);
+        scanf("%*[^\n]");
+        scanf("%*c");
+
         init_params(&params);
+
+        if (p == 'Y' || p == 'y')
+        {
+            double lx = 0, rx = 0, step = 0;
+            do
+            {
+                do_read_double(&lx, "left border of the interval x");
+                do_read_double(&rx, "right border of the interval x");
+                do_read_double(&step, "step");
+            } while (graph(params, lx, rx, step) != 0);
+        };
+
         print_solution(params, &roots);
     };
 }
